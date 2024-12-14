4df18f4880f34f62387aa6af14b73a0e8508a0bb
Megh <meghd9@gmail.com>
2024-12-14 16:40:54 +0000

    Initial project setup and member class

---
+++ src/Member.java
@@ -0,0 +1,44 @@
+public class Member {
+    private String name;
+    private String id;
+    private int phone;
+
+    public Member(String name, String id, int phone) {
+        this.name = name;
+        this.id = id;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+}
